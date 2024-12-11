6fa0e356d74336e72a4025d9c412166602d1f0ba
Punith-H <hpunith68@gmail.com>
2024-12-11 01:23:07 -0800

    Create Number divisible 2 and 3.py

---
+++ Number divisible 2 and 3.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number"))
+if num % 2 == 0:
+           print("divisible by 2")
+elif num % 3 == 0:
+           print("divisible by 3")
+else:
+           print("not divisible by 2 and 3")
