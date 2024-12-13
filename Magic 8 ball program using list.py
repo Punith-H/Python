1d07b04ed1e6e552249471a632a1936137cb50e3
Punith-H <hpunith68@gmail.com>
2024-12-13 02:33:44 -0800

    Create Magic 8 ball program using list.py

---
+++ Magic 8 ball program using list.py
@@ -0,0 +1,13 @@
+import random
+response = [
+    "It is certain",
+    "Replay hazy, try again",
+    "yes, definately",
+    "Ask again later",
+    "my replay is no",
+    "Outlook not so good",
+    "Very doubtful"
+]
+print("ask the magic 8 ball a question: ")
+input()
+print(random.choice(response))
